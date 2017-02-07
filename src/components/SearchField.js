8de4895ddf6bc8a0b9fbbe03e6ab03a1c2e4ba9e
Victor Olsson <victor@meridium.se>
2017-02-07 14:45:18 +0100

    Add search field to search page

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import './SearchField.css';
+import searchIcon from '../media/search.svg';
 
 export default class SearchField extends Component {
   render() {
     return (
-      null
+      <div className='SearchField'>
+      <form method='get' id='search' action='#' onSubmit={this.handleSearchSubmit}>
+      <input type='search' className='SearchField-input' placeholder='Search' />
+      <input className='SearchField-button' name="searchsubmit" type="image" src={searchIcon} value="Sök" />
+      </form>
+      </div>
     );
+  }
+  handleSearchSubmit() {
+    return false;
   }
 }
