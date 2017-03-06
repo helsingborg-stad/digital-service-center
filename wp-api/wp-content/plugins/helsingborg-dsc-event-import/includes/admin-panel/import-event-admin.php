54c5027ac6a4e1a3e083b43f110a1774ed214856
Daniel Bäckelin <daniel@meridium.se>
2017-03-06 15:51:17 +0100

    Disable scheduled job temporarily

--- wp-api/wp-content/plugins/helsingborg-dsc-event-import/includes/admin-panel/import-event-admin.php
+++ wp-api/wp-content/plugins/helsingborg-dsc-event-import/includes/admin-panel/import-event-admin.php
@@ -8,7 +8,7 @@ add_action('admin_menu', 'helsingborg_dsc_event_import_admin_menu');
 function helsingborg_dsc_event_import_admin_menu(){
         add_menu_page( $helsingborg_api_event_import, 'Import events', 'manage_options', 'helsingborg-dsc-event-import', 'helsingborg_dsc_event_import_admin_init' );
         add_action('admin_init', 'register_scheduled_event_properties');
-        add_action('admin_init', 'activate_scheduled_event_import');
+        //add_action('admin_init', 'activate_scheduled_event_import');
 }
  
 function helsingborg_dsc_event_import_admin_init(){
