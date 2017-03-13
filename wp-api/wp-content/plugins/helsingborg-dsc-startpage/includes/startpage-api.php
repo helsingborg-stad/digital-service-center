c63a6e96163b96e1cb3c7c18e68ab1ab88011a7e
Victor Olsson <victor@meridium.se>
2017-03-13 12:26:56 +0100

    Make startpage tags dynamic

--- wp-api/wp-content/plugins/helsingborg-dsc-startpage/includes/startpage-api.php
+++ wp-api/wp-content/plugins/helsingborg-dsc-startpage/includes/startpage-api.php
@@ -15,6 +15,19 @@ function helsingborg_dsc_startpage_get_today_events() {
   return array_values(array_slice($today_posts, 0, 10));
 }
 
+function get_categories_for_posts($posts) {
+  $category_ids = array_map(function($post) {
+    return wp_get_post_categories($post->ID);
+  }, $posts);
+  $category_ids_reduced = [];
+  array_walk_recursive($category_ids, function($a) use (&$category_ids_reduced) { if (!in_array($a, $category_ids_reduced)) {$category_ids_reduced[] = $a; } });
+  $categories_excluding_vistor_and_local = array_values(array_diff($category_ids_reduced, [
+    intval(get_option('hdsc-startpage-setting-visitor-category')),
+    intval(get_option('hdsc-startpage-setting-local-category'))
+  ]));
+  return $categories_excluding_vistor_and_local;
+}
+
 function helsingborg_dsc_startpage_response() {
     return rest_ensure_response([
       backgroundUrl => get_option('hdsc-startpage-setting-background-url'),
@@ -24,14 +37,13 @@ function helsingborg_dsc_startpage_response() {
         return [name => $page->post_title, href => $page->post_name];
       }, get_option('hdsc-startpage-setting-top-links', [])),
       visitorHeading => get_option('hdsc-startpage-setting-visitor-heading', 'Visitor'),
-      visitorTags => [
-        [name => 'See & Do'],
-        [name => 'Attractions'],
-        [name => 'Lorem'],
-        [name => 'Recommendations'],
-        [name => 'Lorem'],
-        [name => 'Lorem'],
-      ],
+      visitorTags => array_map(function($category_id) {
+        $category = get_category($category_id);
+        return [
+          name => $category->name,
+          href => '/visitor/' . $category->slug
+        ];
+      }, get_categories_for_posts(get_posts([ post_type => 'imported_event', numberposts => -1, category => get_option('hdsc-startpage-setting-visitor-category', '')]))),
       visitorPosts => array_map(function($post) {
         return [
           heading  => $post->post_title,
@@ -41,14 +53,13 @@ function helsingborg_dsc_startpage_response() {
         ];
       }, get_posts([ post_type => 'imported_event', posts_per_page => 10, category => get_option('hdsc-startpage-setting-visitor-category', '')])),
       localHeading => get_option('hdsc-startpage-setting-local-heading', 'Local'),
-      localTags => [
-        [name => 'See & Do'],
-        [name => 'Attractions'],
-        [name => 'Lorem'],
-        [name => 'Recommendations'],
-        [name => 'Lorem'],
-        [name => 'Lorem'],
-      ],
+      localTags => array_map(function($category_id) {
+        $category = get_category($category_id);
+        return [
+          name => $category->name,
+          href => '/local/' . $category->slug
+        ];
+      }, get_categories_for_posts(get_posts([ post_type => 'imported_event', numberposts => -1, category => get_option('hdsc-startpage-setting-local-category', '')]))),
       localPosts => array_map(function($post) {
         return [
           heading  => $post->post_title,
@@ -58,14 +69,13 @@ function helsingborg_dsc_startpage_response() {
         ];
       }, get_posts([ post_type => 'imported_event', posts_per_page => 10, category => get_option('hdsc-startpage-setting-local-category', '')])),
       todayHeading => get_option('hdsc-startpage-setting-today-heading', 'Today'),
-      todayTags => [
-        [name => 'See & Do'],
-        [name => 'Attractions'],
-        [name => 'Lorem'],
-        [name => 'Recommendations'],
-        [name => 'Lorem'],
-        [name => 'Lorem'],
-      ],
+      todayTags => array_map(function($category_id) {
+        $category = get_category($category_id);
+        return [
+          name => $category->name,
+          href => '/today/' . $category->slug
+        ];
+      }, get_categories_for_posts(helsingborg_dsc_startpage_get_today_events())),
       todayPosts => array_map(function($post) {
         return [
           heading  => $post->post_title,
