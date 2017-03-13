4eab7f426309a4e1ce63cf42ae9471cfa84406dd
Victor Olsson <victor@meridium.se>
2017-03-13 15:44:35 +0100

    Include both imported and editable events in API
    Also refactored the code a bit with helper functions
    to make it more DRY.

--- wp-api/wp-content/plugins/helsingborg-dsc-startpage/includes/startpage-api.php
+++ wp-api/wp-content/plugins/helsingborg-dsc-startpage/includes/startpage-api.php
@@ -1,8 +1,10 @@
 <?php
 
+// Gets all events which are occurring today
 function helsingborg_dsc_startpage_get_today_events() {
-  $posts = get_posts([ post_type => 'imported_event', numberposts => -1]);
-  $today_posts = array_filter($posts, function($post) {
+  $imported_posts = get_posts([ post_type => 'imported_event', numberposts => -1]);
+  $editable_posts = get_posts([ post_type => 'editable_event', numberposts => -1]);
+  $today_imported_posts = array_filter($imported_posts, function($post) {
     $post_meta = get_post_meta($post->ID, 'imported_event_data', true);
     $door_times = array_map(function($occasion) {
       return $occasion->door_time;
@@ -12,9 +14,17 @@ function helsingborg_dsc_startpage_get_today_events() {
     }));
     return $is_any_door_time_today;
   });
-  return array_values(array_slice($today_posts, 0, 10));
+  $today_editable_posts = array_filter($editable_posts, function($post) {
+    $occasions = get_post_meta($post->ID, 'occasions', false);
+    $door_time = $occasions[0]['door_time'];
+    $is_door_time_today = date('Y-m-d') == date('Y-m-d', strtotime($door_time));
+    return $is_door_time_today;
+  });
+  $all_today_posts = array_merge((array)$today_imported_posts, (array)$today_editable_posts);
+  return array_values(array_slice($all_today_posts, 0, 10));
 }
 
+// Gets all distinct categories for a list of posts (excluding 'Visitor' and 'Local')
 function get_categories_for_posts($posts) {
   $category_ids = array_map(function($post) {
     return wp_get_post_categories($post->ID);
@@ -28,12 +38,50 @@ function get_categories_for_posts($posts) {
   return $categories_excluding_vistor_and_local;
 }
 
+// Gets the shortform of a post's content
 function get_preamble($post_content) {
   $contains_more_html_comment = preg_match('/<!--more-->/', $post_content);
   if ($contains_more_html_comment) {
     $post_content = substr($post_content, 0, strpos($post_content, "<!--more-->"));
   }
   return substr(html_entity_decode($post_content), 0, 100);
+}
+
+// Helper function to map the posts to the correct format for the API response
+function post_mapping_helper($post, $type) {
+  $response = [
+    heading  => html_entity_decode($post->post_title),
+    preamble => get_preamble($post->post_content),
+    href     => '/' . $type . '/' . $post->post_name,
+  ];
+  $thumbnail_url = get_the_post_thumbnail_url($post->ID);
+  if ($thumbnail_url) {
+    $response['imgUrl'] = $thumbnail_url;
+  }
+  return $response;
+}
+
+// Gets imported and editable events for either 'visitor' or 'local' category, and returns a merged array
+function get_visitor_or_local_posts($type) {
+  $imported_posts = get_posts([ post_type => 'imported_event', posts_per_page => 10, category => get_option('hdsc-startpage-setting-' . $type . '-category', '')]);
+  $editable_posts = get_posts([ post_type => 'editable_event', posts_per_page => 10, category => get_option('hdsc-startpage-setting-' . $type . '-category', '')]);
+  $all_posts = array_merge($imported_posts, $editable_posts);
+  return array_map(function($post) use ($type) {
+    return post_mapping_helper($post, $type);
+  }, $all_posts);
+}
+
+function get_visitor_or_local_tags($type) {
+  $imported_events = get_posts([ post_type => 'imported_event', numberposts => -1, category => get_option('hdsc-startpage-setting-' . $type . '-category', '')]);
+  $editable_events = get_posts([ post_type => 'editable_event', numberposts => -1, category => get_option('hdsc-startpage-setting-' . $type . '-category', '')]);
+  $all_events = array_merge($imported_events, $editable_events);
+  return array_map(function($category_id) use ($type) {
+    $category = get_category($category_id);
+    return [
+      name => html_entity_decode($category->name),
+      href => '/' . $type . '/' . $category->slug
+    ];
+  }, get_categories_for_posts($all_events));
 }
 
 function helsingborg_dsc_startpage_response() {
@@ -44,38 +92,15 @@ function helsingborg_dsc_startpage_response() {
         $page = get_post($pageId);
         return [name => $page->post_title, href => $page->post_name];
       }, get_option('hdsc-startpage-setting-top-links', [])),
+
       visitorHeading => get_option('hdsc-startpage-setting-visitor-heading', 'Visitor'),
-      visitorTags => array_map(function($category_id) {
-        $category = get_category($category_id);
-        return [
-          name => html_entity_decode($category->name),
-          href => '/visitor/' . $category->slug
-        ];
-      }, get_categories_for_posts(get_posts([ post_type => 'imported_event', numberposts => -1, category => get_option('hdsc-startpage-setting-visitor-category', '')]))),
-      visitorPosts => array_map(function($post) {
-        return [
-          heading  => html_entity_decode($post->post_title),
-          preamble => get_preamble($post->post_content),
-          href     => '/visitor/' . $post->post_name,
-          imgUrl   => get_the_post_thumbnail_url($post->ID)
-        ];
-      }, get_posts([ post_type => 'imported_event', posts_per_page => 10, category => get_option('hdsc-startpage-setting-visitor-category', '')])),
+      visitorTags => get_visitor_or_local_tags('visitor'),
+      visitorPosts => get_visitor_or_local_posts('visitor'),
+
       localHeading => get_option('hdsc-startpage-setting-local-heading', 'Local'),
-      localTags => array_map(function($category_id) {
-        $category = get_category($category_id);
-        return [
-          name => html_entity_decode($category->name),
-          href => '/local/' . $category->slug
-        ];
-      }, get_categories_for_posts(get_posts([ post_type => 'imported_event', numberposts => -1, category => get_option('hdsc-startpage-setting-local-category', '')]))),
-      localPosts => array_map(function($post) {
-        return [
-          heading  => html_entity_decode($post->post_title),
-          preamble => get_preamble($post->post_content),
-          href     => '/local/' . $post->post_name,
-          imgUrl   => get_the_post_thumbnail_url($post->ID)
-        ];
-      }, get_posts([ post_type => 'imported_event', posts_per_page => 10, category => get_option('hdsc-startpage-setting-local-category', '')])),
+      localTags => get_visitor_or_local_tags('local'),
+      localPosts => get_visitor_or_local_posts('local'),
+
       todayHeading => get_option('hdsc-startpage-setting-today-heading', 'Today'),
       todayTags => array_map(function($category_id) {
         $category = get_category($category_id);
@@ -85,13 +110,8 @@ function helsingborg_dsc_startpage_response() {
         ];
       }, get_categories_for_posts(helsingborg_dsc_startpage_get_today_events())),
       todayPosts => array_map(function($post) {
-        return [
-          heading  => html_entity_decode($post->post_title),
-          preamble => get_preamble($post->post_content),
-          href     => '/today/' . $post->post_name,
-          imgUrl   => get_the_post_thumbnail_url($post->ID)
-        ];
-      }, helsingborg_dsc_startpage_get_today_events())
+        return post_mapping_helper($post, 'today');
+      }, helsingborg_dsc_startpage_get_today_events()),
     ]);
 }
 
