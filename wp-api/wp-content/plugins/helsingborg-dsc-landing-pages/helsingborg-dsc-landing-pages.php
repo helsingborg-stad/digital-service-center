23900452dff21684512e5a2a95f7b8744b353e9d
Victor Olsson <victor@meridium.se>
2017-03-20 09:31:49 +0100

    Add settings page for landing pages

---
+++ wp-api/wp-content/plugins/helsingborg-dsc-landing-pages/helsingborg-dsc-landing-pages.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin Name: Helsingborg Landing pages
+Description: Settings for landing pages
+Author: Meridium
+Version: 0.1
+Author URI: http://meridium.se
+Text Domain: Helsingborg-dsc-landing-pages
+*/
+
+function helsingborg_dsc_landing_pages() {
+  load_plugin_textdomain( 'Helsingborg-dsc-landing-pages' );
+}
+
+include('includes/landing-pages-settings.php');
+
+?>
